9a4a5e75c5a7ea66973b6e8394d041c9f99de59d
Seth Baughman <sbaughman1@me.com>
2016-07-07 13:56:11 -0400

    Add hashing of password using bcrypt before save

--- models/user.js
+++ models/user.js
@@ -1,13 +1,17 @@
 var db = require('../config/db');
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
 
 var UserSchema = db.Schema({
   name: String,
   username: String,
+  password: String,
   updated_at: { type: Date, default: Date.now },
   created_at: { type: Date, default: Date.now }
 });
 
 UserSchema.pre('save', function(next) {
+  this.password = bcrypt.hashSync(this.password, salt);
   next();
 });
 
